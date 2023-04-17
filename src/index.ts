79dd2ec4cc4f31eeedac76d879f8c84cab3faad6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-04-17 12:20:14 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,44 @@
 import http from 'http';
-import express from 'express';
+import express, { Express } from 'express';
 import { Server } from 'socket.io';
 import { io as ioClient } from 'socket.io-client';
+
+export
+class BigbServer {
+  public constructor(private readonly port: number) {
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.ioServer = new Server(this.server, { cors: { origin: '*' } });
+  }
+
+  private readonly app!: Express;
+  private readonly server!: http.Server;
+  private readonly ioServer!: Server;
+
+  public Start(
+    welcome = 'bigbServer',
+    expressApp?: (app: Express) => void,
+    socketIOApp?: (ioServer: Server) => void,
+  ) {
+    expressApp?.(this.app);
+    socketIOApp?.(this.ioServer);
+    this.app.get('/welcome', (req, res) => res.send(welcome));
+    this.server.listen(this.port);
+  }
+}
 
 export
 class Bigb {
   public constructor(portRange: [number, number]) { }
 
-  
+  public createServer() {
+
+  }
+
+  public createSubscriber() {
+
+  }
 }
 
-const app = express();
-const server = http.createServer(app);
-const ioServer = new Server(server, { cors: { origin: '*' } });
-server.listen(3000);
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Hello World!' });
-});
-
-ioServer.on('connection', (socket) => {
-  console.log(socket.id, '服务端连接');
-});
-
-setTimeout(() => {
-  const client = ioClient('http://localhost:3000');
-  client.on('connect', () => {
-    console.log(client.id, '客户端连接');
-  });
-}, 5000);
+const b = new BigbServer(19999);
+b.Start();
