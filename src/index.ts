36cadbdb48cc2fb09fd4fb349e10696d1f1a489e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-04-17 11:42:56 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import http from 'http';
+import express from 'express';
+import SocketIO from 'socket.io';
 
-console.log('你好，世界');
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIO(server);
+server.listen(3000);
