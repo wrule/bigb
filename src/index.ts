d3ee5ead26b982217c23e7baf3a5e45cf3f0d78e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-04-17 11:58:06 +0800

    http和socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import http from 'http';
 import express from 'express';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
+import { io as ioClient } from 'socket.io-client';
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIO(server);
+const ioServer = new Server(server, { cors: { origin: '*' } });
 server.listen(3000);
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Hello World!' });
+});
+
+ioServer.on('connection', (socket) => {
+  console.log(socket.id, '服务端连接');
+});
+
+setTimeout(() => {
+  const client = ioClient('http://localhost:3000');
+  client.on('connect', () => {
+    console.log(client.id, '客户端连接');
+  });
+}, 5000);
